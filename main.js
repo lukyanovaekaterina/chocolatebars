ed3174a2d93ad5215760379982858676f05323e1
Ekaterina Lukyanova <katrin2006.06@inbox.ru>
2019-12-16 14:45:04 +0300

    4

--- main.js
+++ main.js
@@ -31,3 +31,25 @@ $(document).ready(function() {
     owl.trigger("next.owl.carousel");
   });
 });
+
+// review
+
+let reviewsFoto = document.querySelectorAll(".reviews__foto");
+let reviewItems = document.querySelectorAll(".reviews__item");
+
+for (let i = 0; i < reviewsFoto.length; i++){
+reviewsFoto[i].addEventListener("click", function () {
+reviewsFoto[i].classList.add("reviews__foto--activ");
+let reviewsFotoActiv = document.querySelector(".reviews__foto--activ");
+reviewsFotoActiv.classList.remove("reviews__foto--activ");
+reviewItems[i].classList.add("reviews__item--activ");
+let reviewItemsActiv = document.querySelector(".reviews__item--activ");
+//reviewItemsActiv.classList.remove("reviews__item--activ");
+
+});
+}
+
+
+//bar
+
+
