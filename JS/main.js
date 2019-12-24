330b7fecbdc8fa2c0f7ca62bc8bcd960fcef83ff
Ekaterina Lukyanova <katrin2006.06@inbox.ru>
2019-12-24 13:07:48 +0300

    5

--- JS/main.js
+++ JS/main.js
@@ -118,13 +118,16 @@ ymaps.ready(init);
             center: [55.76, 37.64],
             // Уровень масштабирования. Допустимые значения:
             // от 0 (весь мир) до 19.
-            zoom: 15
+            zoom: 13
         });
-      }
+      
         myMap.behaviors.disable("scrollZoom");
-        {
+        
           myPlacemark1 = new ymaps.Placemark(
             [55.7464, 37.5989],
+            {
+              hintContent: "CHO CCO",
+            },
           
           {
             iconLayout: "default#image",
@@ -134,5 +137,53 @@ ymaps.ready(init);
           );
 
         myMap.geoObjects.add(myPlacemark1);
+
+        myPlacemark2 = new ymaps.Placemark(
+          [55.7342, 37.6207],
+          {
+            hintContent: "CHO CCO",
+          },
+        
+        {
+          iconLayout: "default#image",
+          iconImageHref: "./../img/icon/1.png",
+          iconImageSize: [60, 60],
+        }
+        );
+
+      myMap.geoObjects.add(myPlacemark2);
+
+      myPlacemark3 = new ymaps.Placemark(
+        [55.7612, 37.6089],
+        {
+          hintContent: "CHO CCO",
+        },
+      
+      {
+        iconLayout: "default#image",
+        iconImageHref: "./../img/icon/1.png",
+        iconImageSize: [60, 60],
+      }
+      );
+
+    myMap.geoObjects.add(myPlacemark3);
+
+    myPlacemark4 = new ymaps.Placemark(
+      [55.7578, 37.5641],
+      {
+        hintContent: "CHO CCO",
+      },
+    
+    {
+      iconLayout: "default#image",
+      iconImageHref: "./../img/icon/1.png",
+      iconImageSize: [60, 60],
     }
+    );
+
+  myMap.geoObjects.add(myPlacemark4);
+
+
+
+        }
   
